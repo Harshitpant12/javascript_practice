0baf8e09f0e94c109608a325d87362fd8b58b9e7
Harshitpant12 <harshpant778@gmail.com>
2026-02-06 14:00:00 +0530

    callbacks and promises

---
+++ callbacks&promises/promises.js
@@ -0,0 +1,38 @@
+console.log('These are promises');
+
+let prom1 = new Promise((resolve, reject) => {
+    let a = Math.random()
+    if (a < 0.5) {
+        reject("No random number was not supporting you")
+    }
+    else {
+        setTimeout(() => {
+            console.log('Yes I am done');
+            resolve("Harshit")
+        }, 3000);
+    }
+})
+let prom2 = new Promise((resolve, reject) => {
+    let a = Math.random()
+    if (a < 0.5) {
+        reject("No random number was not supporting you 2")
+    }
+    else {
+        setTimeout(() => {
+            console.log('Yes I am done 2');
+            resolve("Harshit 2")
+        }, 1000);
+    }
+})
+
+
+let p3 = Promise.any([prom1, prom2])
+p3.then((a) => {
+    console.log(a);
+}).catch((e) => {
+    console.log(e);
+})
+
+// prom1.then((a) => {
+//     console.log(a);
+// }).catch((err) => console.log(err))
---
+++ callbacks&promises/script.js
@@ -0,0 +1,23 @@
+console.log("Starting...");
+
+setTimeout(() => {
+    console.log("I am in timeout");
+}, 0);
+setTimeout(() => {
+    console.log("I am in timeout");
+}, 0);
+
+console.log("The End");
+
+const callback = (arg) => {
+    console.log(arg);
+}
+
+const loadScript = (src, callback) => {
+    let sc = document.createElement('script');
+    sc.src = src
+    sc.onload = callback("Harshit")
+    document.head.append(sc)
+}
+
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", callback)
