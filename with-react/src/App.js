16da0037ad6813a98f998a1ddcb42e0f1dfc0ae3
Harshitpant12 <harshpant778@gmail.com>
2026-02-07 12:51:26 +0530

    react basics

--- with-react/src/App.js
+++ with-react/src/App.js
@@ -1,12 +1,16 @@
 import { useState } from 'react';
 import "./App.css"
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   const [value, setValue] = useState(0);
   return (
     <div className="App">
+      <Navbar logoText="Harshit"/>
       <div className='value'>{value}</div>
       <button onClick={() => {setValue(value + 1)}}>Click me</button>      
+      <Footer/>
     </div>
   );
 }
