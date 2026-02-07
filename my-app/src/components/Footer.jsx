f54445efa68a15831c835c6b19bef349d6bf3bbe
Harshitpant12 <harshpant778@gmail.com>
2026-02-07 14:38:17 +0530

    React with vite

---
+++ my-app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      Copyright &copy; pantji.in | All rights reserved
+    </div>
+  )
+}
+
+export default Footer
